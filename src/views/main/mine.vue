e4e7fbcadd34a1c2301310d1c0073a2ebe5da2fa
gxx <562606139@qq.com>
2018-03-06 11:09:55 +0800

    0306-gxx

--- src/views/main/mine.vue
+++ src/views/main/mine.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="mine">
-    <div class="head">
-      <img src="" alt="" class="id_icon">
-      <div class="name">
-        <h2><span>{{item.name}}</span><img src="" alt=""></h2>
-        <p>{{item.complany}}</p>
-        <p>手机号 <span>{{item.phone}}</span></p>
+    <div class="head flex">
+      <img :src="item.icon" alt="" class="head_icon">
+      <div class="msg">
+        <h2 class="f44"><span>{{item.name}}</span><img src="" alt=""></h2>
+        <p class="f24">{{item.complany}}</p>
+        <p class="f24">手机号  {{item.phone}}</p>
       </div>
-      <img src="" alt="" class="qrcode_icon">
+      <img src="../../assets/main/mine/qrcode.png" alt="" class="qrcode_icon">
     </div>
+    <ul class="item" v-for="i in menu">
+      <li v-for="j in i" class="flex" @click="$go(`/webapp/mine/${j.url}`)">
+        <img :src="require(`@/assets/main/mine/${j.icon}.png`)" alt="" class="icon">
+        <span class="name f32">{{j.name}}</span>
+        <span class="f28 color_font-s" v-if="!j.url">敬请期待</span>
+        <img src="../../assets/common/arrow.png" alt="" class="arrow">
+      </li>
+    </ul>
     <foot-tip/>
   </div>
 </template>
@@ -20,10 +28,56 @@
     data() {
       return {
         item: {
+          icon: require('@/assets/main/mine/head.png'),
           name: '贞子问',
           complany: '中赢卓信财富投资管理(北京)有限公司闵行分公司',
           phone: '17211112222'
-        }
+        },
+       menu: [[{
+        icon: 'sweep',
+        name: '扫一扫添加客户',
+        url: 'qrcode'
+      },{
+        icon: 'sweep',
+        name: '扫一扫添加理财师',
+         url: ''
+      },{
+        icon: 'mine',
+        name: '我的理财师',
+         url: ''
+      }],[{
+        icon: 'have',
+        name: '已有客户',
+         url: ''
+      },{
+        icon: 'customer',
+        name: '潜在客户',
+         url: ''
+      }],[{
+        icon: 'advertisement',
+        name: '广告卓信产品记录',
+         url: ''
+      },{
+        icon: 'advertisement',
+        name: '针对客户展业记录',
+         url: ''
+      },{
+        icon: 'share',
+        name: '邀约客户投资记录',
+         url: ''
+      },{
+        icon: 'discount',
+        name: '赠送客户优惠记录',
+         url: ''
+      }], [{
+        icon: 'commission',
+        name: '我的佣金',
+         url: ''
+      }], [{
+        icon: 'seting',
+        name: '设置',
+         url: ''
+      }]]
       }
     },
     created() {
@@ -39,4 +93,35 @@
 </script>
 
 <style lang="sass" scoped>
+  .mine
+    .head
+      padding: .76rem .4rem .6rem
+      align-items: flex-start
+      color: #fff
+      background: rgb(50,153,209)
+      .head_icon
+        width: 1.16rem
+      .msg
+        flex: 1
+        padding: 0 .4rem
+        p
+          margin-top: .1rem
+          line-height: .34rem
+      .qrcode_icon
+        width: .72rem
+    .item
+      padding: 0 .4rem
+      background: #fff
+      margin-top: .4rem
+      li
+        height: 1.16rem
+        .icon
+          width: .56rem
+        .name
+          padding: 0 .4rem
+          color: #252525
+          flex: 1
+        .arrow
+          padding-left: .2rem
+          height: .34rem
 </style>
---
+++ src/views/mine/qrcode.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="qrcode">
+    <div class="head flex">
+      <img :src="item.icon" alt="" class="head_icon">
+      <div class="msg">
+        <h2 class="f36"><span>{{item.name}}</span><img src="" alt=""></h2>
+        <p class="f24">{{item.complany}}</p>
+        <p class="f44 flex"><img src="../../assets/main/mine/phone.png" alt="">  {{item.phone}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'qrcode',
+    data() {
+      return {
+        item: {
+          icon: require('@/assets/main/mine/head.png'),
+          name: '贞子问',
+          complany: '中赢卓信财富投资管理(北京)有限公司闵行分公司',
+          phone: '17211112222'
+        },
+      }
+    },
+    created() {
+    },
+    components: {
+    },
+    methods: {
+    },
+    watch: {
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+  .qrcode
+    .head
+      padding: .76rem .4rem .6rem
+      align-items: flex-start
+      .head_icon
+        width: 1.16rem
+      .msg
+        flex: 1
+        padding-left: .4rem
+        p
+          margin-top: .1rem
+          line-height: .34rem
+          &.flex
+            justify-content: flex-start
+          img
+            width: .72rem
+            padding-right: .1rem
+</style>
