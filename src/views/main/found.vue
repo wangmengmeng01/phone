97f14fc4e9b173ea1db96f5f08753f2044ce6d15
gxx <562606139@qq.com>
2018-03-19 11:11:09 +0800

    0319-gxx

--- src/views/main/found.vue
+++ src/views/main/found.vue
@@ -4,9 +4,15 @@
     <div class="item mygift">
       <h2 class="subtitle"></h2>
       <h3>我的礼物</h3>
-      <div class="">
-
+      <div class="con">
+        <ul>
+          <li></li>
+        </ul>
       </div>
+    </div>
+    <!--履行承诺-->
+    <div>
+
     </div>
   </div>
 </template>
